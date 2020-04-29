69f81836bd01857cfd721a9dc924496491ede0ed
whatiamdoing <alwayswannafly070400@mail.ru>
2020-04-29 14:12:00 +0300

    Реализован презентер

--- app/src/main/java/com/whatiamdoing/weather/presentation/presenter/MainPresenter.kt
+++ app/src/main/java/com/whatiamdoing/weather/presentation/presenter/MainPresenter.kt
@@ -1,5 +1,36 @@
 package com.whatiamdoing.weather.presentation.presenter
 
-class MainPresenter {
+import com.whatiamdoing.weather.data.CurrentWeatherEntity
+import com.whatiamdoing.weather.model.WeatherData
+import com.whatiamdoing.weather.presentation.ui.MainView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import moxy.InjectViewState
+import moxy.MvpPresenter
 
+@InjectViewState
+class MainPresenter : MvpPresenter<MainView>() {
+
+    override fun onFirstViewAttach() {
+        viewState.getCurrentLocation()
+    }
+
+    fun loadData(lat: Float, long: Float) {
+        CoroutineScope(Dispatchers.Main).launch {
+            viewState.showProgressBar()
+            when (val result = loadWeather(lat, long)) {
+                null -> viewState.onError()
+                else -> viewState.setData(result)
+            }
+            viewState.hideProgressBar()
+            viewState.saveDataInPrefs(lat, long)
+        }
+    }
+
+    private suspend fun loadWeather(lat: Float, long: Float): CurrentWeatherEntity? =
+        withContext(CoroutineScope(Dispatchers.IO).coroutineContext) {
+            WeatherData.getDataFromApi(lat, long)
+        }
 }
